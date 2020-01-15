e644f9b5d9cffbcbf5afd6c5cc7352d031682b84
arikr84 <34011638+arikr84@users.noreply.github.com>
2020-01-15 22:46:42 +0200

    Delete fibonacci_sum.py

--- Exercises/fibonacci_sum.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter the 1st integer in the Fibonacci sequence: "))
-num2 = int(input("Enter the 2nd integer in the Fibonacci sequence: "))
-
-list = [num1,num2]
-while list[-1] < 10000:
-    i = list[-1] + list[-2]
-    list.append(i)
-
-print("\nEach number is the sum of the two preceding ones.\nThe 1st and 2nd numbers will determine the rest of the sequence.\n\nFibonacci sequence: " + str(list))
