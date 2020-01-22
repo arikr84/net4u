991f21ef7eecbb641673490e392ab9fbe8b91968
Arik <arik.rozenman@nokia.com>
2020-01-22 16:59:05 +0200

    The hotels madness continues

--- Exercises/hotels.py
+++ Exercises/hotels.py
@@ -60,7 +60,7 @@ def max_guests_capacity():
         hotels = json.loads(hotels)
         if not re.search("'day': \[]", line):
             guest_capacity = guest_capacity + list(hotels.values())[2]
-    return guest_capacity // 2
+    return guest_capacity
 
 def reservation():
     temp_directory = tempfile.gettempdir()
@@ -73,37 +73,71 @@ def reservation():
         quit("You have requested to host " + str(guests) + " guests which is more than our maximum capacity (" + str
         (guest_capacity) + ")")
 
-    rooms = int(input(Style.BRIGHT + "[Please enter how many room you wish to acquire]: "))
-    if rooms > guests:
-        quit("The number of requested rooms (" + str(rooms) + ") can't be lower than the number of guests (" +
-             str(guests) + ").")
-
-    for line in open(str(temp_directory) + "/hotels.txt", "r").readlines():
-        if not re.search("'day': \[]", line):
-            available_rooms_counter = available_rooms_counter + 1
-    available_rooms_counter = available_rooms_counter // 2
-    if rooms > available_rooms_counter:
-        quit("You requested more rooms (" + str(rooms) + ") than our maximum available rooms (" +
-             str(available_rooms_counter) + ").")
-
     nights = int(input(Style.BRIGHT + "[Please enter how many nights you wish to stay]: "))
     for line in open(str(temp_directory) + "/hotels.txt", "r").readlines():
         file = line.replace("'", "\"")
         file = json.loads(file)
         if nights > len(list(file.values())[4]) - 1:
             nights_counter = nights_counter + 1
-    nights_counter = nights_counter // 2
+    nights_counter = nights_counter
     if nights_counter == 8:
         quit("Couldn't find a single room that is available for " + str(nights) + " night(s)")
 
-    breakfast = input(Style.BRIGHT + "[You wish to enjoy our breakfast in exchange for 50$ per night (Yes/No)]: ").lower()
+    rooms = int(input(Style.BRIGHT + "[Please enter how many rooms you wish to acquire]: "))
+    if rooms > guests:
+        quit("The number of requested rooms (" + str(rooms) + ") can't be higher than the number of guests (" +
+             str(guests) + ").")
+
+    guests_per_room = guests / rooms
+    if guests_per_room > 3.0:
+        quit("To allocate " + str(guests) + " guests within " + str(rooms) + " rooms means that we have to split " +
+             "more than 3 guests per room while our biggest room can only accommodate maximum of 3 guests.")
+    available_rooms = open(str(temp_directory) + "/available_rooms.txt", "w+")
+    available_rooms.write("")
+    for line in open(str(temp_directory) + "/hotels.txt", "r").readlines():
+        hotels = line.replace("'", "\"")
+        hotels = json.loads(hotels)
+        available_rooms = open(str(temp_directory) + "/available_rooms.txt", "a+")
+        if int(len(list(hotels.values())[4])) > nights:
+            # print(Fore.LIGHTBLUE_EX + str(hotels))
+            available_rooms.write("{}\n".format(str(hotels)))
+            # print("aaaaaaaaaaaaaaaaaaaaaaa" + str(list(available_rooms.values())[3]))
+    a = list(hotels.values())[3]
+    print("aaaaaaaaaa" + str(a))
+    available_rooms.close()
+    for line in open(str(temp_directory) + "/available_rooms.txt", "r").readlines():
+        available_rooms = line.replace("'", "\"")
+        available_rooms = json.loads(available_rooms)
+        print("BBBB" + str(list(available_rooms.values())[3]))
+        if list(available_rooms.values())[3] < a:
+            b = list(available_rooms.values())[3]
+
+
+
+    print("dddddddddddd" + str(b))
+
+
+
+
+
+
+
+    for line in open(str(temp_directory) + "/hotels.txt", "r").readlines():
+        if not re.search("'day': \[]", line):
+            available_rooms_counter = available_rooms_counter + 1
+    available_rooms_counter = available_rooms_counter
+    if rooms > available_rooms_counter:
+        quit("You requested more rooms (" + str(rooms) + ") than our maximum available rooms (" +
+             str(available_rooms_counter) + ").")
+
+    breakfast = input(Style.BRIGHT + "[You wish to enjoy our breakfast in exchange for extra 50$ per night (Yes/No)]: ").lower()
     while breakfast != "yes" and breakfast != "no":
         breakfast = input(Style.BRIGHT + "[Please enter 'Yes' or 'No']: ").lower()
 
     reservation = open(str(temp_directory) + "/reservation.txt", "w+")
-
 files_creation()
 menu()
+
 #
 # guests / rooms == how many people inside 1 room.  if return > 3 it's not possible since max room size is 3
 # if == 3 need to check if there are available rooms for 3 people ---- for example if 6 guests want 2 rooms I need to find 2 rooms that can fill each 3 people
@@ -115,3 +149,4 @@ menu()
 #4 The calculation will yield number of requires rooms (X). check which 2 people rooms are the cheapers
 #5 if leftovers people calcule the leftovers // 3           -  add them people to the rooms for 2 people
 #6 The calculation will yield number of requires rooms (X). check which 2 people rooms are the cheapers
+
