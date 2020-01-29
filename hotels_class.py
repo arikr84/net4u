932b0521a9cf8d7cb48f7de2232aa783310211c7
Arik Rozenman <arikr84@gmail.com>
2020-01-29 08:55:40 +0200

    commit lists examples

---
+++ hotels_class.py
@@ -0,0 +1,8 @@
+class Hotels():
+    def __init__(self, hotels, check_in, check_out, rooms, adults, children):
+        self.hotels = hotels
+        self.check_in = check_in
+        self.check_out = check_out
+        self.rooms = rooms
+        self.adults = children
+
