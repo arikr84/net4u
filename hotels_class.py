66391f00911f8533a632477c2f0f05b90d9c46ab
Arik Rozenman <arikr84@gmail.com>
2020-02-01 23:26:53 +0200

    commit lists examples

--- hotels_class.py
+++ hotels_class.py
@@ -3,7 +3,7 @@ import tempfile
 import json
 import re
 import random
-from colorama import Fore, init, Style, Cursor
+from colorama import Fore, init, Style
 import csv
 import os
 import datetime
@@ -19,7 +19,7 @@ site_name = 'Trivago'
 
 
 class Hotels:
-    def __init__(self, check_in, check_out, hotel, guests, rooms, breakfast, name, phone):
+    def __init__(self, check_in, check_out, hotel, guests, rooms, breakfast, name, phone, id_number):
         # check_in_dates_list = ['2.8.2020', '3.8.2020', '4.8.2020', '5.8.2020']
         full_dates_list = ['2.8.2020', '3.8.2020', '4.8.2020', '5.8.2020', '6.8.2020']
         full_days_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
@@ -58,14 +58,7 @@ class Hotels:
                                "- Enter 'Isrotel and Fattal' to search for the cheapest rooms from both"
                                " Isrotel and Fattal\n\nEnter your selection: ").lower()
 
-        # available_rooms_per_day = self.available_rooms_per_day(self)
-        # print(Style.BRIGHT + "\n\tNumber of available rooms per day in " + str(
-        #     self.hotel).title() + ":\n\t" + Style.NORMAL + Fore.LIGHTBLUE_EX +
-        #       str(available_rooms_per_day).title().replace("{", "").replace("}", "").replace("'", "") + Fore.BLACK +
-        #       "\n\tThe maximum number of rooms that are available for you between " + str(
-        #     self.check_in) + " and " + str(self.check_out) + " is: " + Style.BRIGHT + Fore.RED + str(min_rooms_value))
-
-        available_guests_per_day = self.available_guests_per_day()
+        available_guests_per_day = self.available_guests_per_day(self)
         print(Style.BRIGHT + "\n\tNumber of people we can accommodate per day in " + str(
             self.hotel).title() + ":\n\t" + Fore.LIGHTBLUE_EX + Style.NORMAL +
               str(available_guests_per_day).title().replace("{", "").replace("}", "").replace("'", "") + Fore.BLACK +
@@ -82,7 +75,8 @@ class Hotels:
         for n in selected_days_index_list:
             while list(available_guests_per_day.values())[n] < self.guests:
                 self.guests = int(input("On " + full_days_list[n] + " (" + full_dates_list[n] +
-                                        ") we can only accommodate maximum of " + str(list(available_guests_per_day.values())[n]) +
+                                        ") we can only accommodate maximum of " + str(
+                    list(available_guests_per_day.values())[n]) +
                                         " guests while you requested to host " + str(self.guests) +
                                         " guests.\nPlease change the amount of guests accordingly.\n"
                                         "Enter new number of guests: "))
@@ -118,12 +112,13 @@ class Hotels:
         self.order_acceptance(self)
 
         self.name = input('Full Name: ')
-        self.phone = int(input('Phone Number: (e.g. 0501234567)'))
+        self.phone = int(input('Phone Number (e.g. 0501234567): '))
+        self.id_number = int(input('Identification Number (e.g. 039954263): '))
 
     @staticmethod
     def loading(sec):
         for i in range(sec):
-            print('.' * 5, end='')
+            print('.' * 7, end='')
             sleep(1)
         print("\n")
 
@@ -172,7 +167,7 @@ class Hotels:
         # using the library os.path can check if a file exist or not
         if os.path.isfile(rooms_csv_file):
             # if entered the if condition, the file exists
-            print(temp_directory + '/available_rooms.csv already exists')
+            print(str(rooms_csv_file) + ' already exists')
             pass
         else:
             # if entered the else condition the file does not exists and will be created
@@ -207,7 +202,7 @@ class Hotels:
                                '2.8.2020': 'available', '3.8.2020': 'available', '4.8.2020': 'available',
                                '5.8.2020': 'available', '6.8.2020': 'available'})
 
-                print(temp_directory + '/available_rooms.csv created')
+                print(str(rooms_csv_file) + ' created')
 
     @staticmethod
     def available_rooms_per_day(self):
@@ -281,7 +276,7 @@ class Hotels:
         return days_rooms_dict
 
     @staticmethod
-    def available_guests_per_day():
+    def available_guests_per_day(self):
         global min_guests_value
         sunday = 0
         monday = 0
@@ -293,16 +288,56 @@ class Hotels:
             file = csv.reader(f)
             next(file)
             for row in file:
-                if 'available' in row[4]:
-                    sunday = sunday + int(row[2])
-                if 'available' in row[5]:
-                    monday = monday + int(row[2])
-                if 'available' in row[6]:
-                    tuesday = tuesday + int(row[2])
-                if 'available' in row[7]:
-                    wednesday = wednesday + int(row[2])
-                if 'available' in row[8]:
-                    thursday = thursday + int(row[2])
+                if self.hotel == 'isrotel':
+                    if 'available' in row[4] and 'Isrotel' in row[0]:
+                        sunday = sunday + int(row[2])
+                if self.hotel == 'fattal':
+                    if 'available' in row[4] and 'fattal' in row[0]:
+                        sunday = sunday + int(row[2])
+                if self.hotel == 'isrotel and fattal':
+                    if 'available' in row[4]:
+                        sunday = sunday + int(row[2])
+
+                if self.hotel == 'isrotel':
+                    if 'available' in row[5] and 'Isrotel' in row[0]:
+                        monday = monday + int(row[2])
+                if self.hotel == 'fattal':
+                    if 'available' in row[5] and 'fattal' in row[0]:
+                        monday = monday + int(row[2])
+                if self.hotel == 'isrotel and fattal':
+                    if 'available' in row[5]:
+                        monday = monday + int(row[2])
+
+                if self.hotel == 'isrotel':
+                    if 'available' in row[6] and 'Isrotel' in row[0]:
+                        tuesday = tuesday + int(row[2])
+                if self.hotel == 'fattal':
+                    if 'available' in row[6] and 'fattal' in row[0]:
+                        tuesday = tuesday + int(row[2])
+                if self.hotel == 'isrotel and fattal':
+                    if 'available' in row[6]:
+                        tuesday = tuesday + int(row[2])
+
+                if self.hotel == 'isrotel':
+                    if 'available' in row[7] and 'Isrotel' in row[0]:
+                        wednesday = wednesday + int(row[2])
+                if self.hotel == 'fattal':
+                    if 'available' in row[7] and 'fattal' in row[0]:
+                        wednesday = wednesday + int(row[2])
+                if self.hotel == 'isrotel and fattal':
+                    if 'available' in row[7]:
+                        wednesday = wednesday + int(row[2])
+
+                if self.hotel == 'isrotel':
+                    if 'available' in row[8] and 'Isrotel' in row[0]:
+                        thursday = thursday + int(row[2])
+                if self.hotel == 'fattal':
+                    if 'available' in row[8] and 'fattal' in row[0]:
+                        thursday = thursday + int(row[2])
+                if self.hotel == 'isrotel and fattal':
+                    if 'available' in row[8]:
+                        thursday = thursday + int(row[2])
+
         days_guests_dict['sunday'] = sunday
         days_guests_dict['monday'] = monday
         days_guests_dict['tuesday'] = tuesday
@@ -320,6 +355,10 @@ class Hotels:
                 if 'available' in row[4]:
                     prices.append(row[3])
             prices = sorted(prices)
+
+            print("\nThe following are the cheapest " + str(
+                self.rooms) + " rooms we could find in " + self.hotel.title() + ":\n" + str('-' * 64))
+
         for n in range(self.rooms):
             with open(rooms_csv_file, 'r') as f:
                 file = csv.reader(f)
@@ -327,12 +366,12 @@ class Hotels:
                 for row in file:
                     if prices[n] in row[3] and 'available' in row[4]:
                         if self.breakfast == 'yes':
-                            print(i[4] + "  |  Hotel: " + row[0], " |  Room: " + row[1],
-                                  " |  Maximum guests allowed: " + row[2], " |  Price: " + str(int(prices[n]) +
-                                                                                               breakfast_price) + "$")
+                            print(Fore.LIGHTBLUE_EX + "Hotel: " + row[0], " |  Room: " + row[1],
+                                  " |  Room Guests Maximum Capacity: " + row[2], " |  Price: " +
+                                  str(int(prices[n]) + breakfast_price) + "$")
                         else:
-                            print(i[4] + "  |  Hotel: " + row[0], " |  Room: " + row[1],
-                                  " |  Maximum guests allowed: " + row[2], " |  Price: " + prices[n] + "$")
+                            print(Fore.LIGHTBLUE_EX + "Hotel: " + row[0], " |  Room: " + row[1],
+                                  " |  Room Guests Maximum Capacity: " + row[2], " |  Price: " + prices[n] + "$")
 
     @staticmethod
     def max_guests_capacity_per_day():
@@ -416,10 +455,20 @@ class Hotels:
             order_acceptance = int(input(Fore.LIGHTRED_EX + str(
                 order_acceptance) + "is not a valid input.\n- Enter (1) to accept and continue with the reservation\n"
                                     "- Enter (2) to go back to the the initial input details section\n\n"
-                                    "Enter your selection: " + Style.NORMAL + ": ").lower())
+                                    "Enter your selection: " + Style.NORMAL).lower())
         if order_acceptance == 1:
             print("Thank you for choosing " + site_name + ". Please wait few seconds while we redirect you to the "
                                                           "reservation process")
+
             self.loading(sec=10)
+            with open(rooms_csv_file, 'w', newline='') as csvfile:
+                file = csv.DictWriter(csvfile,
+                                      fieldnames=['Hotel', 'Room', 'Guests', 'Price', '2.8.2020', '3.8.2020',
+                                                  '4.8.2020',
+                                                  '5.8.2020', '6.8.2020'])
+                file.writeheader()
+                file.writerow({'Hotel': 'Isrotel', 'Room': 1, 'Guests': 2, 'Price': random.randrange(250, 450),
+                               '2.8.2020': 'available', '3.8.2020': 'available', '4.8.2020': 'available',
+                               '5.8.2020': 'available', '6.8.2020': 'available'})
         else:
             user_input = Hotels('check_in', 'check_out', 'hotel', 'rooms', 'breakfast', 'guests', 'name', 'phone')
